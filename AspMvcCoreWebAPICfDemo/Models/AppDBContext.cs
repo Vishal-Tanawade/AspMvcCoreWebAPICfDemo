adcd32a103fe84048b88c0193996c8df1175dfe3
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-31 23:18:33 +0530

    Created ModelBuilderExtensions and AppDBContext class

---
+++ AspMvcCoreWebAPICfDemo/Models/AppDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspMvcCoreWebAPICfDemo.Models
+{
+    public class AppDBContext: DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options): base(options) { }
+
+        protected  override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Seed();
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ AspMvcCoreWebAPICfDemo/Models/ModelBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspMvcCoreWebAPICfDemo.Models
+{
+    public static  class ModelBuilderExtensions
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                
+                new Product() { ProductCode=101,ProductName="Speaker",ProductPrice=30000.00m},
+                new Product() { ProductCode = 102, ProductName = "Desktop", ProductPrice = 30000.00m }, 
+                new Product() { ProductCode = 103, ProductName = "Laptops", ProductPrice = 25000.00m }, 
+                new Product() { ProductCode = 104, ProductName = "Gaming Pc's", ProductPrice = 60000.00m }, 
+                new Product() { ProductCode = 105, ProductName = "Servers", ProductPrice = 100000.00m }, new Product() { ProductCode = 106, ProductName = "Tablets", ProductPrice = 5000.00m }
+
+
+
+                );
+        }
+    }
+}
