d370326dc8491013354ed3cfef6ee22ad85de9c3
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-31 17:07:15 +0530

    Poco Class added

---
+++ AspMvcCoreWebAPICfDemo/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AspMvcCoreWebAPICfDemo.Models
+{
+    public class Product
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductCode { get; set; }
+        [Required(ErrorMessage ="Product Name Should not be blank")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage ="Product price should not be blank")]
+        public decimal ProductPrice { get; set; }
+    }
+}
